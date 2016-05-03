5a31cd9993f9b1f550b43930936a161113af98de
Bahul Jain <bkj2111@columbia.edu>
2016-05-03 13:43:39 -0400

    hello jsjs

---
+++ README.md
@@ -0,0 +1,12 @@
+# JSJS #
+
+## Build & Run ##
+
+```sh
+$ cd JSJS
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/jsjs/app/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package com.jsjs.app
+
+import org.scalatra._
+
+class MyScalatraServlet extends JsjsStack {
+
+	/*	get("/") {
+		<html>
+			<body>
+				<h1>Hello from JSJS!</h1>
+				Say<a href="hello-scalate">hello to Scalate</a>
+				.
+			</body>
+		</html>
+	}
+*/
+}
