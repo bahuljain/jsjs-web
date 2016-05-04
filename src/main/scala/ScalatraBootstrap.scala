fe65477e8ec4698604f0f842666cffd5e723aed2
Prakhar Srivastav <prakhar1989@gmail.com>
2016-05-04 18:15:23 -0400

    correct access control for API calls.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -4,6 +4,10 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    // adding CORS support to allow cross domain access from the
+    // following domains
+    val domains = "http://localhost:8001 http://jsjs.surge.sh"
+    context.initParameters("org.scalatra.cors.allowedOrigins") = domains
     context.mount(new MyScalatraServlet, "/*")
   }
 }
--- src/main/scala/com/jsjs/app/MyScalatraServlet.scala
+++ src/main/scala/com/jsjs/app/MyScalatraServlet.scala
@@ -1,10 +1,11 @@
 package com.jsjs.app
 
 import org.scalatra._
+import org.scalatra.CorsSupport
 import org.json4s.{ DefaultFormats, Formats }
 import org.scalatra.json._
 
-class MyScalatraServlet extends JsjsStack with JacksonJsonSupport {
+class MyScalatraServlet extends JsjsStack with JacksonJsonSupport with CorsSupport {
 	protected implicit lazy val jsonFormats: Formats = DefaultFormats
 	// Sets up automatic case class to JSON output serialization, required by
 	//	protected implicit lazy val jsonFormats: Formats = DefaultFormats
